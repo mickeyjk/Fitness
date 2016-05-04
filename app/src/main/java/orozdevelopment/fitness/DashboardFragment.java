215771284f8bb5c89b4acf660e21420c782d5e21
mickeyjk <michael@Michaels-MacBook-Air.local>
2016-05-04 15:16:21 -0400

    adding recycle view

--- app/src/main/java/orozdevelopment/fitness/DashboardFragment.java
+++ app/src/main/java/orozdevelopment/fitness/DashboardFragment.java
@@ -3,6 +3,8 @@ package orozdevelopment.fitness;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +17,7 @@ import android.view.ViewGroup;
  */
 public class DashboardFragment extends Fragment {
 
-
+    private RecyclerView rv;
     public DashboardFragment() {
         // Required empty public constructor
     }
@@ -34,16 +36,21 @@ public class DashboardFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-
+        if (getArguments() == null) {
+            return;
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View v = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        rv = (RecyclerView)v.findViewById(R.id.rv);
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        return v;
     }
 
 }
